902237fc263a04a2c0291598e26c929d0011d45f
Arthu085 <arthurghizi22@gmail.com>
2025-08-20 10:12:40 -0300

    adicionado mui no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Button from '@mui/material/Button'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 function App() {
 
   return (
     <>
+    <h1>Plate Vision</h1>
+    <Button variant='contained' color='primary'>
+      Clique aqui
+    </Button>
+    <Button startIcon={<DeleteIcon/>}></Button>
     </>
   )
 }
